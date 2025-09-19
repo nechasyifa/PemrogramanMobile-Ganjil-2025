3edb69b9783e7c0e51e3038fe3fc4f267996f32e
nechasyifa <nechasyifasyafitri@gmail.com>
2025-09-19 11:21:21 +0700

    JS4_P5_Langkah3

--- Week4/JS4_Praktikum5.dart
+++ Week4/JS4_Praktikum5.dart
@@ -2,3 +2,9 @@ void main() {
   var record = ('first', a: 2, b: true, 'last');
   print(record);
 }
+
+// Langkah 3
+(int, int) tukar((int, int) record) {
+  var (a, b) = record;
+  return (b, a);
+}
