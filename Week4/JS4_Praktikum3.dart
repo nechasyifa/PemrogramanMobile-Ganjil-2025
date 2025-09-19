610c31020d1ac806246654edcc312d8fd1373694
nechasyifa <nechasyifasyafitri@gmail.com>
2025-09-19 07:29:28 +0700

    JS4_P3_Langkah3.1

--- Week4/JS4_Praktikum3.dart
+++ Week4/JS4_Praktikum3.dart
@@ -8,17 +8,24 @@ void main() {
 
   var nobleGases = {2: 'helium', 10: 'neon', 18: 2};
 
-  print(gifts);
-  print(nobleGases);
-
   //Langkah 3
+
+  gifts['nama'] = 'Necha';
+  gifts['nim'] = '2341720167';
+
+  nobleGases[20] = 'Necha';
+  nobleGases[21] = '2341720167';
+
   var mhs1 = Map<String, String>();
-  gifts['first'] = 'partridge';
-  gifts['second'] = 'turtledoves';
-  gifts['fifth'] = 'golden rings';
+  mhs1['nama'] = 'Necha';
+  mhs1['nim'] = '2341720167';
 
   var mhs2 = Map<int, String>();
-  nobleGases[2] = 'helium';
-  nobleGases[10] = 'neon';
-  nobleGases[18] = 'argon';
+  mhs2[1] = 'Necha';
+  mhs2[2] = '2341720167';
+
+  print("gifts: $gifts");
+  print("nobleGases: $nobleGases");
+  print("mhs1: $mhs1");
+  print("mhs2: $mhs2");
 }
