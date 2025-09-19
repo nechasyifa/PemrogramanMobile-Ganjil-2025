49be6914e12d884f8573a6e99c786826daeb7d04
nechasyifa <nechasyifasyafitri@gmail.com>
2025-09-19 11:53:39 +0700

    JS4_P5_Langkah4

--- Week4/JS4_Praktikum5.dart
+++ Week4/JS4_Praktikum5.dart
@@ -1,6 +1,11 @@
 void main() {
   var record = ('first', a: 2, b: true, 'last');
   print(record);
+
+  // Langkah 4
+  // Record type annotation in a variable declaration:
+  (String, int) mahasiswa = ('Necha', 2341720167);
+  print(mahasiswa);
 }
 
 // Langkah 3
