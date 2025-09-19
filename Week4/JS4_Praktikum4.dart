c94909bc9df42ddf6996e55634965865e5fb51a0
nechasyifa <nechasyifasyafitri@gmail.com>
2025-09-19 07:45:30 +0700

    JS4_P4_Langkah3

--- Week4/JS4_Praktikum4.dart
+++ Week4/JS4_Praktikum4.dart
@@ -1,7 +1,12 @@
 void main() {
-  var list1 = [1, 2, 3];
-  var list2 = [0, ...list1];
-  print(list1);
+  var list = [1, 2, 3];
+  var list2 = [0, ...list];
+  print(list);
   print(list2);
   print(list2.length);
+
+  var list1 = [1, 2, null];
+  print(list1);
+  var list3 = [0, ...?list1];
+  print(list3.length);
 }
