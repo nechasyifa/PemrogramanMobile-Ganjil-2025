66b69316e00a2cf7bf81247593bf73fcde702a43
nechasyifa <nechasyifasyafitri@gmail.com>
2025-10-07 14:37:08 +0700

    JS6_P1_Langkah4

--- Week6/layout_flutter/lib/main.dart
+++ Week6/layout_flutter/lib/main.dart
@@ -7,11 +7,43 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Langkah 4
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* soal 2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+          /* soal 3*/
+          const Icon(Icons.star, color: Colors.red),
+          const Text('41'),
+        ],
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Necha Syifa Syafitri - 2341720167',
       home: Scaffold(
         appBar: AppBar(title: const Text('Flutter layout demo')),
-        body: const Center(child: Text('Hello World')),
+        body: Column(children: [titleSection]),
       ),
     );
   }
