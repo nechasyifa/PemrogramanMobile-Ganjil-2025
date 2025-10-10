77fab2833a090558a1bcbaee82323f4f353584b1
nechasyifa <nechasyifasyafitri@gmail.com>
2025-10-11 00:14:56 +0700

    JS7_Praktikum_Langkah5

--- Week7/flutter_plugin_pubdev/lib/red_text_widget.dart
+++ Week7/flutter_plugin_pubdev/lib/red_text_widget.dart
@@ -4,7 +4,7 @@ import 'package:auto_size_text/auto_size_text.dart';
 class RedTextWidget extends StatelessWidget {
   final String text;
 
-  const RedTextWidget({Key? key, required this.text,}) : super(key: key);
+  const RedTextWidget({Key? key, required this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
